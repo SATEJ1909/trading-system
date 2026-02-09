6d8155f0e7e6969c05a19109287eb9b03cb1b4b7
Satej G.  Niswade <satejniswade6@gmail.com>
2026-02-09 11:57:54 +0530

    fixed : dependancies

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,6 +1,6 @@
 import z from 'zod'
 import type { Request, Response } from 'express'
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import UserModel from '../models/user.js'
 import JWT_SECRET from '../config.js'
