5ed16301d55a487874900abde735810140a28308
Satej G.  Niswade <satejniswade6@gmail.com>
2026-02-09 12:06:54 +0530

    fixed : scripts

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,6 +1,6 @@
 import z from 'zod'
 import type { Request, Response } from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import UserModel from '../models/user.js'
 import JWT_SECRET from '../config.js'
